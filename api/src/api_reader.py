33f020000bf1bd90ae3e6a1a484d2bc2b6756b74
Bogdan <I@gmail.com>
2025-09-30 20:31:47 +0300

    Little changes in api

--- api/src/api_reader.py
+++ api/src/api_reader.py
@@ -3,9 +3,7 @@ import requests
 filename='set'
 
 def search_github_repos(query: str, max_results: int = 5):
-    """
-    Ищет репозитории на GitHub по заданному запросу и возвращает список словарей с названием и ссылкой.
-    """
+
     url = "https://api.github.com/search/repositories"
     params = {
         "q": query,
@@ -15,24 +13,24 @@ def search_github_repos(query: str, max_results: int = 5):
 
     try:
         response = requests.get(url, params=params, timeout=10)
-        response.raise_for_status()  # Проверяем HTTP-код ответа
+        response.raise_for_status()
     except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP ошибка: {http_err}")
+        print(f"HTTP error: {http_err}")
         return []
     except requests.exceptions.ConnectionError as conn_err:
-        print(f"Ошибка соединения: {conn_err}")
+        print(f"Error connection: {conn_err}")
         return []
     except requests.exceptions.Timeout as timeout_err:
-        print(f"Превышено время ожидания: {timeout_err}")
+        print(f"Timeout is too over: {timeout_err}")
         return []
     except requests.exceptions.RequestException as req_err:
-        print(f"Ошибка запроса: {req_err}")
+        print(f"Request error: {req_err}")
         return []
 
     try:
         data = response.json()
     except ValueError as json_err:
-        print(f"Ошибка декодирования JSON: {json_err}")
+        print(f"Error of decoding json: {json_err}")
         return []
 
     repos = []
@@ -51,11 +49,11 @@ def main():
         query = [line.strip() for line in f if line.strip()]
 
     print(query)
-    print(f"Поиск репозиториев GitHub по запросу: '{query}'\n")
+    print(f"Looking for repos : '{query}'\n")
     repos = search_github_repos(query)
 
     if not repos:
-        print("Репозитории не найдены или произошла ошибка.")
+        print("Repos weere not found or an error appeared.")
         return
 
     for idx, repo in enumerate(repos, start=1):
